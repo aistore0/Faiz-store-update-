7ded9e15ba4f94f5d6e9ab000c0ad77be8a2d582
Faizbot1 <faisgaming131@gmail.com>
2024-06-04 23:52:08 +0900

    Add files via upload

---
+++ media/sticker/thumb/aibotzz/panel.js
@@ -0,0 +1,8 @@
+const domain = '' //Faiz.anjim.my.id
+const apikey = '' //apikey plta
+const capikey = '' //apikey pltc
+const eggsnya = '15' 
+const location = '1' 
+
+module.exports = { domain, apikey, capikey, eggsnya, location }
+
